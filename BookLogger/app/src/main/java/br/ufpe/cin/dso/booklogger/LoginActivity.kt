25c07f04bbd8a3ec44f89c372f6b567b88dbfdf3
Danielle Sales <dso@cin.ufpe.br>
2018-12-05 00:42:34 -0300

    Login with Firebase

--- BookLogger/app/src/main/java/br/ufpe/cin/dso/booklogger/LoginActivity.kt
+++ BookLogger/app/src/main/java/br/ufpe/cin/dso/booklogger/LoginActivity.kt
@@ -4,14 +4,38 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
 import android.content.Intent
+import com.google.firebase.auth.FirebaseAuth
+import android.util.Log
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+    private val TAG = "LoginActivity"
+    private var mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        btn_login.setOnClickListener { startActivity(Intent(applicationContext, MainActivity::class.java)) }
+        btn_login.setOnClickListener {
+            var email = txt_login_email?.text.toString()
+            var password = txt_login_password?.text.toString()
+            login(email, password)
+        }
         btn_signup.setOnClickListener { startActivity(Intent(applicationContext, SignUpActivity::class.java)) }
+    }
+
+    fun login(email: String, password: String){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful) {
+                        Log.d(TAG, "signInWithEmail:success")
+                        startActivity(Intent(applicationContext, MainActivity::class.java))
+                    } else {
+                        Log.e(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(this@LoginActivity, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+
+                    }
+                }
     }
 }
