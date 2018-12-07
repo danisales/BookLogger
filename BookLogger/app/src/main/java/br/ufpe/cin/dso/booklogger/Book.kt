4de24f14d1c67d2f00fee0e94bf48d8ef62bc312
Danielle Sales <dso@cin.ufpe.br>
2018-12-06 21:43:33 -0300

    Return search result as a list of books

---
+++ BookLogger/app/src/main/java/br/ufpe/cin/dso/booklogger/Book.kt
@@ -0,0 +1,8 @@
+package br.ufpe.cin.dso.booklogger
+
+class Book (val id: String,
+            val title: String,
+            val author: String,
+            val thumbnail: String?,
+            val borrowed: Boolean,
+            val status: String?)
--- BookLogger/app/src/main/java/br/ufpe/cin/dso/booklogger/GoogleBooksRequest.kt
+++ BookLogger/app/src/main/java/br/ufpe/cin/dso/booklogger/GoogleBooksRequest.kt
@@ -10,9 +10,9 @@ class GoogleBooksRequest {
     private fun request(url: String) : JSONObject {
         val (request, response, result) = url.httpGet().responseString()
 
-        /*var obj = JSONObject(result.get())
-        var kind = obj.getString("kind")
-        Log.d(TAG, kind)*/
+        //var obj = JSONObject(result.get())
+        //var volumeInfo = obj.getJSONObject("volumeInfo")
+        //Log.d(TAG, volumeInfo.getString("title"))
 
         return JSONObject(result.get())
     }
@@ -28,5 +28,33 @@ class GoogleBooksRequest {
 
     fun get(bookId: String) : JSONObject {
         return request("https://www.googleapis.com/books/v1/volumes/" + bookId)
+    }
+
+    fun results(query: String) : List<Book>{
+        var result = search(query)
+        var items = result.getJSONArray("items")
+        var numItems = 10
+        val listItems = ArrayList<Book>()
+
+        (0..numItems-1).forEach { i ->
+            var item = items.getJSONObject(i)
+
+            var id = item.getString("id")
+            var volumeInfo = item.getJSONObject("volumeInfo")
+            var title = volumeInfo.getString("title")
+            var author = volumeInfo.getJSONArray("authors").getString(0)
+            var imageLinks: JSONObject? = null
+            var thumbnail: String? = null
+            try{
+                imageLinks = volumeInfo.getJSONObject("imageLinks")
+                thumbnail = imageLinks.getString("smallThumbnail")
+            } catch(e: Exception){}
+            var borrowed = false
+            var status = null
+
+            var book = Book(id, title, author, thumbnail, borrowed, status)
+            listItems.add(book)
+        }
+        return listItems
     }
 }
