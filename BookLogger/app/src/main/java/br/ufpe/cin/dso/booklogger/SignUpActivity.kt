e6b28e24b71f417e85e38b8e19b9d050689be9df
Danielle Sales <dso@cin.ufpe.br>
2018-12-09 00:35:53 -0300

    Save user on database

--- BookLogger/app/src/main/java/br/ufpe/cin/dso/booklogger/SignUpActivity.kt
+++ BookLogger/app/src/main/java/br/ufpe/cin/dso/booklogger/SignUpActivity.kt
@@ -6,15 +6,20 @@ import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
 import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     private val TAG = "SignUpActivity"
     private var mAuth = FirebaseAuth.getInstance()
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        database = FirebaseDatabase.getInstance().reference
 
         btn_signup_submit.setOnClickListener {
             var email = txt_signup_email?.text.toString()
@@ -24,15 +29,29 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     @SuppressLint("ShowToast")
-    fun signUp(email:String, password: String){
+    private fun signUp(email:String, password: String) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful()) {
                         Log.d(TAG, "createUserWithEmail:success")
+                        onAuthSuccess(task.result.user)
                     } else {
                         Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(this, "Registration failed", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "Algo deu errado", Toast.LENGTH_SHORT).show()
                     }
                 }
+    }
+
+    private fun onAuthSuccess(user: FirebaseUser){
+        var id = user.uid
+        var email = user.email
+        try {
+            database.child("users").child(id).child("email").setValue(email)
+            Toast.makeText(this, "Cadastro feito com sucesso", Toast.LENGTH_SHORT).show()
+            finish()
+        } catch(e: Exception){
+            Toast.makeText(this, "Algo deu errado", Toast.LENGTH_SHORT).show()
+            Log.d(TAG, e.message)
+        }
     }
 }
