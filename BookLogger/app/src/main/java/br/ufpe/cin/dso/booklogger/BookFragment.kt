43b943fcd46f700e6e781bd0283e23c3830ff287
Danielle Sales <dso@cin.ufpe.br>
2018-12-12 18:46:50 -0300

    Using Service to load data from Firebase

--- BookLogger/app/src/main/java/br/ufpe/cin/dso/booklogger/BookFragment.kt
+++ BookLogger/app/src/main/java/br/ufpe/cin/dso/booklogger/BookFragment.kt
@@ -1,12 +1,17 @@
 package br.ufpe.cin.dso.booklogger
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
 import android.support.annotation.RequiresApi
 import android.support.v4.app.Fragment
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -15,11 +20,11 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.fragment_book.*
 import kotlinx.android.synthetic.main.fragment_book.view.*
+import org.jetbrains.anko.support.v4.startService
 
 class BookFragment : Fragment() {
     val TAG = "BookFragment"
     private var mAuth = FirebaseAuth.getInstance()
-    private var user = mAuth.currentUser
     private lateinit var database: DatabaseReference
 
     @RequiresApi(Build.VERSION_CODES.M)
@@ -27,12 +32,14 @@ class BookFragment : Fragment() {
         var view = inflater.inflate(R.layout.fragment_book, container, false)
         database = FirebaseDatabase.getInstance().reference
 
-        Thread(){
-            var status = arguments!!.get("STATUS") as String
-            this.requireActivity().runOnUiThread {
-                getItems(status)
-            }
-        }.start()
+        LocalBroadcastManager
+                .getInstance(this.requireActivity())
+                .registerReceiver(resultReceiver, IntentFilter("FirebaseBroadcast"))
+
+        var service = Intent(activity, FirebaseService::class.java)
+        var status = arguments!!.get("STATUS") as String
+        service.putExtra("STATUS", status)
+        activity!!.startService(service)
 
         view.btn_reading_add.setOnClickListener {
             requireActivity().startActivity(Intent(activity, SearchBookActivity::class.java))
@@ -41,30 +48,27 @@ class BookFragment : Fragment() {
         return view
     }
 
-    fun getItems(status: String) {
-        var reqActivity = this.requireActivity()
-        val booksListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                var books: ArrayList<Book> = arrayListOf()
-                dataSnapshot.children.forEach {
-                    var book = it.getValue<Book>(Book::class.java)!!
-                    books.add(book)
-                }
-                books = books.filter{x -> x.status.equals(status)} as ArrayList<Book>
+    override fun onPause() {
+        super.onPause()
+        LocalBroadcastManager
+                .getInstance(this.requireActivity())
+                .unregisterReceiver(resultReceiver)
+    }
 
-                //var listBooks = view!!.findViewById(R.id.list_reading_books) as RecyclerView
-                list_reading_books.layoutManager = LinearLayoutManager(reqActivity)
-                list_reading_books.addItemDecoration(DividerItemDecoration(reqActivity, LinearLayoutManager.VERTICAL))
-                list_reading_books.adapter = BookAdapter(books, reqActivity)
-            }
-            override fun onCancelled(databaseError: DatabaseError) {
-                Log.w(TAG, databaseError.toException().message)
-            }
+    private fun setRecyclerView(books: List<Book>){
+        list_reading_books.layoutManager = LinearLayoutManager(this.requireActivity())
+        list_reading_books.addItemDecoration(DividerItemDecoration(this.requireActivity(), LinearLayoutManager.VERTICAL))
+        list_reading_books.adapter = BookAdapter(books, this.requireActivity())
+    }
+
+
+    private val resultReceiver = object : BroadcastReceiver() {
+        @RequiresApi(Build.VERSION_CODES.M)
+        override fun onReceive(context: Context, intent: Intent) {
+            var bundle = intent.extras
+            var items = bundle.getSerializable("RESULT") as List<Book>
+            setRecyclerView(items)
         }
-        database.child("users")
-                .child(user!!.uid)
-                .child("books")
-                .addListenerForSingleValueEvent(booksListener)
     }
 
     companion object {
---
+++ BookLogger/app/src/main/java/br/ufpe/cin/dso/booklogger/FirebaseService.kt
@@ -0,0 +1,58 @@
+package br.ufpe.cin.dso.booklogger
+
+import android.app.IntentService
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+
+
+class FirebaseService : IntentService("FirebaseService") {
+    private val TAG = "FirebaseService"
+    private var mAuth = FirebaseAuth.getInstance()
+    private var user = mAuth.currentUser
+    private lateinit var database: DatabaseReference
+
+    override fun onHandleIntent(intent: Intent?) {
+        database = FirebaseDatabase.getInstance().reference
+
+        Log.d(TAG, "Service running")
+
+        var status = intent!!.extras.getString("STATUS")
+        getItems(status)
+    }
+
+    fun getItems(status: String) {
+        var context = this
+        val booksListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var books: ArrayList<Book> = arrayListOf()
+                dataSnapshot.children.forEach {
+                    var book = it.getValue<Book>(Book::class.java)!!
+                    if(book.status.equals(status)) books.add(book)
+                }
+                //books = books.filter{x -> x.status.equals(status)} as ArrayList<Book>
+
+                var bundle = Bundle()
+                bundle.putSerializable("RESULT", books)
+
+                var broadcast = Intent("FirebaseBroadcast")
+                broadcast.putExtras(bundle)
+                var instance = LocalBroadcastManager.getInstance(context)
+                        .sendBroadcast(broadcast)
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, databaseError.toException().message)
+            }
+        }
+        database.child("users")
+                .child(user!!.uid)
+                .child("books")
+                .addListenerForSingleValueEvent(booksListener)
+    }
+
+}
