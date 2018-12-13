e43ad0eb6f363aa5a08ffbe6d2610b87a6423d17
Danielle Sales <dso@cin.ufpe.br>
2018-12-13 00:22:47 -0300

    Implement Logout option

--- BookLogger/app/src/main/java/br/ufpe/cin/dso/booklogger/SettingsFragment.kt
+++ BookLogger/app/src/main/java/br/ufpe/cin/dso/booklogger/SettingsFragment.kt
@@ -1,11 +1,13 @@
 package br.ufpe.cin.dso.booklogger
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_settings.*
@@ -19,12 +21,20 @@ class SettingsFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         var view = inflater.inflate(R.layout.fragment_settings, container, false)
 
-        var txt_email = view.findViewById(R.id.txt_settings_email) as EditText
+        var txt_email = view.findViewById<EditText>(R.id.txt_settings_email)
         txt_email.isEnabled = false
         txt_email.setText(user!!.email)
 
+        var btn_signout = view.findViewById<Button>(R.id.btn_logout)
+        btn_signout.setOnClickListener {
+            signOut()
+        }
+        
         return view
     }
 
-
+    fun signOut(){
+        mAuth.signOut()
+        this.requireActivity().startActivity(Intent(activity, LoginActivity::class.java))
+    }
 }
